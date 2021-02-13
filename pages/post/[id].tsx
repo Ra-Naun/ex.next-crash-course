618c26e89291fa13caa80397e3251050436ee523
Ra-Naun <dmitriy_slavskiy@mail.ru>
2021-02-13 17:39:39 +0300

    update

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,101 +1,121 @@
-import {useState, useEffect} from 'react'
-import {MainLayout} from '../../components/MainLayout'
-import Link from 'next/link'
-import {useRouter} from 'next/router'
-import {NextPageContext} from 'next'
-import {MyPost} from '../../interfaces/post'
-import {getAllPostIds} from '../../lib/posts'
+import { useState, useEffect } from "react";
+import { MainLayout } from "../../components/MainLayout";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { NextPageContext, GetStaticProps } from "next";
+import { MyPost } from "../../interfaces/post";
+import { getAllPostIds } from "../../lib/posts";
 
 interface PostPageProps {
-  post: MyPost
+    post: MyPost;
 }
 
 export default function Post({ post: serverPost }: PostPageProps) {
-  const [post, setPost] = useState(serverPost)
-  const router = useRouter()
+    const [post, setPost] = useState(serverPost);
+    const router = useRouter();
 
-  useEffect(() => {
-    async function load() {
-      const response = await fetch(`${process.env.API_URL}/posts/${router.query.id}`)
-      const data = await response.json()
-      setPost(data)
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`${process.env.API_URL}/posts/${router.query.id}`);
+            const data = await response.json();
+            setPost(data);
+        }
+
+        if (!serverPost) {
+            load();
+        }
+    }, []);
+
+    if (!post) {
+        return (
+            <MainLayout>
+                <p>Loading ...</p>
+            </MainLayout>
+        );
     }
 
-    if (!serverPost) {
-      load()
-    }
-  }, [])
-
-  if (!post) {
-    return <MainLayout>
-      <p>Loading ...</p>
-    </MainLayout>
-  }
-
-  return(
-    <MainLayout>
-      <h1>{post.title}</h1>
-      <hr />
-      <p>{post.body}</p>
-      <Link href={'/posts'}><a>Back to all posts</a></Link>
-    </MainLayout>
-  )
+    return (
+        <MainLayout>
+            <h1>{post.title}</h1>
+            <hr />
+            <p>{post.body}</p>
+            <Link href={"/posts"}>
+                <a>Back to all posts</a>
+            </Link>
+        </MainLayout>
+    );
 }
 
 interface PostNextPageContext extends NextPageContext {
-  query: {
-    id: string
-  }
+    query: {
+        id: string;
+    };
 }
 
-Post.getInitialProps = async ({ query, req }: PostNextPageContext) => {
-  if (!req) {
-    return {post: null}
-  }
+//U____________________________________________________________________________________________________________
+// Post.getInitialProps = async ({ query, req }: PostNextPageContext) => {
+//     if (!req) {
+//         return { post: null };
+//     }
 
-  const response = await fetch(`${process.env.API_URL}/posts/${query.id}`)
-  const post: MyPost = await response.json()
+//     const response = await fetch(`${process.env.API_URL}/posts/${query.id}`);
+//     const post: MyPost = await response.json();
 
-  return {
-    post
-  }
-}
+//     return {
+//         post,
+//     };
+// };
 
+//ServerSideRendering__________________________________________________________________________________________
 
+// export async function getServerSideProps({ query, req }: PostNextPageContext) {
+//     const response = await fetch(`${process.env.API_URL}/posts/${query.id}`);
+//     const post: MyPost = await response.json();
 
-
-
-
-//__________________________________________________________________________________________
-
-//  export  async function getStaticProps({ params }) {
-  
-
-//   const response = await fetch(`${process.env.API_URL}/posts/${params.id}`)
-//   const post: MyPost = await response.json()
-
-//   return { props: { post:post } }
+//     return {
+//         props: { post },
+//     };
 // }
 
-// export  async function getStaticPaths() {
+//Static Server Rendering__________________________________________________________________________________________
 
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const response = await fetch(`${process.env.API_URL}/posts/${params.id}`);
 
-//   const paths = await getAllPostIds()
-  
-//   return {
-//     paths,
-//     fallback: false
-//   }
-// }
+    const post: MyPost = await response.json();
 
-//__________________________________________________________________________________________
+    // if (!post.id) {
+    //     return {
+    //         redirect: {
+    //             destination: "/",
+    //             permanent: false,
+    //         },
+    //     };
+    // }
 
-// export async function getServerSideProps({ query, req }) {
-//   // if (!req) {
-//   //   return {post: null}
-//   // }
-//   const response = await fetch(`http://localhost:4200/posts/${query.id}`)
-//   const post = await response.json()
-//
-//   return {props: {post}}
-// }
+    // OR
+
+    // if (!post.id) {
+    //   return {
+    //     notFound: true,
+    //   }
+    // }
+
+    return {
+        props: { post },
+        // Next.js will attempt to re-generate the page:
+        // - When a request comes in
+        // - At most once every second
+        revalidate: 1, // In seconds
+    };
+};
+
+export const GetStaticPaths = async () => {
+    const paths = await getAllPostIds();
+    paths.pop(paths[paths.lenth - 1]);
+
+    return {
+        paths,
+        fallback: true,
+    };
+};
